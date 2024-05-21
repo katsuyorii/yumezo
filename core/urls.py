6f25eddaddb4c97927dec3646d00b1e5b5c8f14b
katsuyorii <katsuyoriiii@gmail.com>
2024-05-21 10:11:20 +0300

    Create about, contacts, delivery, refund html pages and views

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, AboutView, DeliveryView, RefundView, ContactsView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('delivery/', DeliveryView.as_view(), name='delivery'),
+    path('refund/', RefundView.as_view(), name='refund'),
+    path('about/', AboutView.as_view(), name='about'),
+    path('contacts/', ContactsView.as_view(), name='contacts'),
 ]
+
--- core/views.py
+++ core/views.py
@@ -2,6 +2,9 @@ from django.views.generic import TemplateView
 from .models import SliderImage, NewsProductImage
 
 
+'''
+Класс-представление для страницы - "Главная".
+'''
 class IndexView(TemplateView):
     template_name = 'core/index.html'
 
@@ -10,3 +13,55 @@ class IndexView(TemplateView):
         context['title'] = 'Аниме магазин Yumezo'
         context['slider_images'] = SliderImage.objects.all()
         context['news_product_images'] = NewsProductImage.objects.first()
+
+
+'''
+Класс-представление для страницы - "О нас".
+'''
+class AboutView(TemplateView):
+    template_name = 'core/about.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'О нас'
+
+        return context
+    
+
+'''
+Класс-представление для страницы - "Оплата и доставка".
+'''
+class DeliveryView(TemplateView):
+    template_name = 'core/delivery.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Оплата и доставка'
+
+        return context
+    
+
+'''
+Класс-представление для страницы - "Возврат и обмен".
+'''
+class RefundView(TemplateView):
+    template_name = 'core/refund.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Возврат и обмен'
+
+        return context
+    
+
+'''
+Класс-представление для страницы - "Контакты".
+'''
+class ContactsView(TemplateView):
+    template_name = 'core/contacts.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Контакты'
+
+        return context
