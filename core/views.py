b602240f6aa31d2b1fe0ea2091436281ea4cbbda
katsuyorii <katsuyoriiii@gmail.com>
2024-05-21 10:00:45 +0300

    Create index.html page and index view

--- core/views.py
+++ core/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+from .models import SliderImage, NewsProductImage
 
-# Create your views here.
+
+class IndexView(TemplateView):
+    template_name = 'core/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Аниме магазин Yumezo'
+        context['slider_images'] = SliderImage.objects.all()
+        context['news_product_images'] = NewsProductImage.objects.first()
